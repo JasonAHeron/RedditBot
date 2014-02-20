acb7668defb780f98032f877942a07484e1c8dc0
Jason Heron <JasonAHeron@Gmail.com>
2014-02-19 21:19:31 -0800

    comment searc mostly working, proof of conecpt for comment search working

--- AACompiled.py
+++ AACompiled.py
@@ -1,7 +1,8 @@
 import praw
 import time
-subreddit_names =  ['hardwareswap', 'buildapcsales']
-search_words =  [['$250', 'I']]
+from tqdm import *
+subreddit_names =  ['hardwareswap']
+search_words =  [['$250']]
 frequency = 20.0
 recipient = "111qq"
 def throwError(error = "unhandled", exit=True, code=0):
@@ -14,7 +15,7 @@ def botInit(username, password, auto=True):
         if auto:
             r.login(username, password)
         else:
-            r.login("111qq","9175Rape")
+            r.login()
     except praw.errors.InvalidUserPass as err:
         throwError(err)
     return r
@@ -26,32 +27,42 @@ def outputResults(r, results, message = False, recipient = "none"):
         for result in results:
             print result
 def commentSearch(r, subreddit_names, searchWords, firstPass, already_done=[]):
+    searchWords = searchWords[0]
     if firstPass:
         first_results = []
     results = []
     for subreddit_name in subreddit_names:
         subreddit = r.get_subreddit(subreddit_name)
-        for submission in subreddit.get_hot(limit=100):
+        for submission in tqdm(subreddit.get_hot(limit=100),"comments",100):
             full_submission = r.get_submission(submission_id=submission.id)
             full_submission.replace_more_comments(limit=16, threshold=10)
             flat_comments = praw.helpers.flatten_tree(full_submission.comments)
             for comment in flat_comments:
                 if not hasattr(comment, 'body'):
                     continue
-                print comment.body
-
-                
-def titleSearchBot(r, subreddit_names, searchWords, frequency, recipient):
-    commentSearch(r, subreddit_names, searchWords, True)
+                comment_text = comment.body.split()
+                for searchWord in searchWords:
+                    for comment_word in comment_text:
+                        if (searchWord.lower() == comment_word.lower()) and comment.id not in already_done:
+                            if not firstPass:
+                                results.append(comment)
+                            else:
+                                first_results.append(subreddit_name.upper() + " :: " + submission.title + " :: " + submission.short_link + "\n" + "    :: " + comment.body)
+                            already_done.append(comment.id)
+    if firstPass:
+        return {'first_results':first_results , 'already_done':already_done}
+    return {'results':results , 'already_done':already_done}
+def commentSearchBot(r, subreddit_names, searchWords, frequency, recipient):
+    results = commentSearch(r, subreddit_names, searchWords, True)
     already_done = results['already_done']
     outputResults(r, results['first_results'])
     while True:
-        results = titleSearch(r, subreddit_names, searchWords, False, already_done)
+        results = commentSearch(r, subreddit_names, searchWords, False, already_done)
         already_done = results['already_done']
         outputResults(r, results['results'], True, recipient)
         time.sleep(max(frequency,20))
 def runBot(subreddit_names, searchWords, frequency, recipient):
     r = botInit("none", "none", False)
-    titleSearchBot(r, subreddit_names, searchWords, frequency, recipient)
+    commentSearchBot(r, subreddit_names, searchWords, frequency, recipient)
 if __name__ == "__main__":
     runBot(subreddit_names, search_words, frequency, recipient)
--- botCore.py
+++ botCore.py
@@ -45,8 +45,8 @@ def compileBot(subreddit_names, search_words, frequency, recipient):
     printFunction(file, throwError)
     printFunction(file, botInit)
     printFunction(file, outputResults)
-    printFunction(file, titleSearch)
-    printFunction(file, titleSearchBot)
+    printFunction(file, commentSearch)
+    printFunction(file, commentSearchBot)
     printFunction(file, runBot)
     print >> file, 'if __name__ == "__main__":'
     print >> file, '    runBot(subreddit_names, search_words, frequency, recipient)'
@@ -80,7 +80,7 @@ def botInit(username, password, auto=True):
 
 def runBot(subreddit_names, searchWords, frequency, recipient):
     r = botInit("none", "none", False)
-    titleSearchBot(r, subreddit_names, searchWords, frequency, recipient)
+    commentSearchBot(r, subreddit_names, searchWords, frequency, recipient)
 
 #-------------------------------------#
 # Result related
@@ -123,19 +123,31 @@ def titleSearch(r, subreddit_names, searchWords, firstPass, already_done=[]):
     return {'results':results , 'already_done':already_done}
 
 def commentSearch(r, subreddit_names, searchWords, firstPass, already_done=[]):
+    searchWords = searchWords[0]
     if firstPass:
         first_results = []
     results = []
     for subreddit_name in subreddit_names:
         subreddit = r.get_subreddit(subreddit_name)
-        for submission in subreddit.get_hot(limit=100):
+        for submission in tqdm(subreddit.get_hot(limit=100),"comments",100):
             full_submission = r.get_submission(submission_id=submission.id)
             full_submission.replace_more_comments(limit=16, threshold=10)
             flat_comments = praw.helpers.flatten_tree(full_submission.comments)
             for comment in flat_comments:
                 if not hasattr(comment, 'body'):
                     continue
-                print comment.body
+                comment_text = comment.body.split()
+                for searchWord in searchWords:
+                    for comment_word in comment_text:
+                        if (searchWord.lower() == comment_word.lower()) and comment.id not in already_done:
+                            if not firstPass:
+                                results.append(comment)
+                            else:
+                                first_results.append(subreddit_name.upper() + " :: " + submission.title + " :: " + submission.short_link + "\n" + "    :: " + comment.body)
+                            already_done.append(comment.id)
+    if firstPass:
+        return {'first_results':first_results , 'already_done':already_done}
+    return {'results':results , 'already_done':already_done}
 
 def commentTitleSearch(r, subreddit_names, searchWords):
     results = titleSearch(r, subreddit_names, searchWords, True)
@@ -162,4 +174,12 @@ def titleSearchBot(r, subreddit_names, searchWords, frequency, recipient):
         outputResults(r, results['results'], True, recipient)
         time.sleep(max(frequency,20))
 
-
+def commentSearchBot(r, subreddit_names, searchWords, frequency, recipient):
+    results = commentSearch(r, subreddit_names, searchWords, True)
+    already_done = results['already_done']
+    outputResults(r, results['first_results'])
+    while True:
+        results = commentSearch(r, subreddit_names, searchWords, False, already_done)
+        already_done = results['already_done']
+        outputResults(r, results['results'], True, recipient)
+        time.sleep(max(frequency,20))
